944e503ab9db2922474ff11097b8a165e12d657c
yoshiyuki.kanno <yoshiyuki.kanno@stoic.co.jp>
2013-01-16 06:13:56 +0000

    Enable multi processes

--- src/resizerl.erl
+++ src/resizerl.erl
@@ -1,13 +1,13 @@
 -module(resizerl).
 
--export([start/0, stop/0, init/1]).
--export([resize/4]).
+-export([start/1, stop/1, init/2]).
+-export([resize/5]).
 
 %%--------------------------------------------------------------------
 %% API
 %%--------------------------------------------------------------------
--spec start() -> pid() | {error, any()}.
-start() ->
+-spec start(atom()) -> pid() | {error, any()}.
+start(Id) ->
     Exe = case code:priv_dir(?MODULE) of
                  {error, bad_name} ->
                      case code:which(?MODULE) of
@@ -19,53 +19,53 @@ start() ->
                  Dir ->
                      filename:join(Dir, ?MODULE_STRING)
              end,
-    spawn(?MODULE, init, [Exe]).
+    spawn(?MODULE, init, [Exe, Id]).
 
 
--spec stop() -> any().
-stop() ->
-    ?MODULE ! stop.
+-spec stop(atom()) -> any().
+stop(Id) ->
+    Id ! stop.
 
--spec init(string()) -> any().
-init(Exe) ->
-    register(?MODULE, self()),
+-spec init(string(), atom()) -> any().
+init(Exe, Id) ->
+    register(Id, self()),
     process_flag(trap_exit, true),
     Port = open_port({spawn, Exe}, [{packet, 4}, binary, exit_status]),
-    loop(Port).
+    loop(Port, Id).
 
--spec resize(string(), binary(), integer(), integer()) -> {ok, binary()} | {error, any()}.
-resize(Path, ImageBin, Width, Height) ->
-    call_port({resize_opencv, Path, ImageBin, Width, Height}).
+-spec resize(atom(), binary(), binary(), integer(), integer()) -> {ok, binary()} | {error, any()}.
+resize(Id, Path, ImageBin, Width, Height) ->
+    call_port(Id, {resize_opencv, Path, ImageBin, Width, Height}).
 
 %%--------------------------------------------------------------------
 %% INNER FUNCTIONS
 %%--------------------------------------------------------------------
--spec call_port(any()) -> any().
-call_port(Msg) ->
-    ?MODULE ! {call, self(), Msg},
+-spec call_port(atom(), any()) -> any().
+call_port(Id, Msg) ->
+    Id ! {call, self(), Msg},
     receive
-        {?MODULE, Result} ->
+        {Id, Result} ->
             Result
     end.
 
--spec loop(port()) -> any().
-loop(Port) ->
+-spec loop(port(), atom()) -> any().
+loop(Port, Id) ->
     receive
         {call, Caller, Msg} ->
             Port ! {self(), {command, term_to_binary(Msg)}},
             receive
                 {Port, {data, Data}} ->
-                    Caller ! {?MODULE, binary_to_term(Data)};
+                    Caller ! {Id, binary_to_term(Data)};
                 {Port, {exit_status, Status}} ->
                     exit({port_terminated, Status});
                 {'EXIT', Port, Reason} ->
                     exit(Reason);
-		Other ->
-		    io:format("received: ~p~n", [Other])
+                Other ->
+                    io:format("received: ~p~n", [Other])
             after 5000 ->
-                Caller ! {?MODULE, {error, timeout}}
+                Caller ! {Id, {error, timeout}}
             end,
-            loop(Port);
+            loop(Port, Id);
         stop ->
             Port ! {self(), close},
             receive
